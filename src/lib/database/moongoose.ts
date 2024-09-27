c75b5b053e89faae94f28a79280e972c8a6c7739
Abhijit1102 <abhijitrajkumar2@gmail.com>
2024-09-27 19:48:48 +0530

    initial part-5

--- src/lib/database/moongoose.ts
+++ src/lib/database/moongoose.ts
@@ -2,31 +2,23 @@ import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
-}
-
-let cached: MongooseConnection = (global as any).mongoose
-
-if(!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
-}
+let connection: { conn: Mongoose | null; promise: Promise<Mongoose> | null } = {
+  conn: null,
+  promise: null,
+};
 
 export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+  if (connection.conn) return connection.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-  cached.promise = 
-    cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
-    })
+  if (!connection.promise) {
+    connection.promise = mongoose.connect(MONGODB_URL, {
+      dbName: 'imaginify',
+      bufferCommands: false,
+    });
+  }
 
-  cached.conn = await cached.promise;
-
-  return cached.conn;
-}
+  connection.conn = await connection.promise;
+  return connection.conn;
+};
